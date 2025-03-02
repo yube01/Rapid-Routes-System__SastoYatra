79faabaa09847fe149c26a958fabb9ed1daed8c3
yube01 <yubraja008@gmail.com>
2025-03-02 13:26:53 +0545

    server setup with drizzle orm

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import db from "./db";
+
+const app = express();
+const port = process.env.PORT ;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.listen(port, async () => {
+  try {
+    console.log(`Server is running on port ${port}`);
+    console.log(db.query)
+
+    app.get("/", (req, res) => {
+      res.send("server running !!");
+    });
+  } catch (error) {
+    console.log("Error");
+  }
+});
